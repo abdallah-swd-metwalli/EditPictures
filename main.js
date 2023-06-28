d7b0c2a429c2130e9a7d2471e611329528dbc673
Abn Sheikh <127383411+AbnSheikh0@users.noreply.github.com>
2023-06-28 14:33:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,60 +1,4 @@
-let saturate = document.getElementById("saturate");
-let contrast = document.getElementById("contrast");
-let brightness = document.getElementById("brightness");
-let sepia = document.getElementById("sepia");
-let grayscale = document.getElementById("grayscale");
-let blur = document.getElementById("blur");
-let hueRotate = document.getElementById("hue-rotate");
-
-let upload = document.getElementById("upload");
-let download = document.getElementById("download");
-let img = document.getElementById("img");
-
-let reset = document.querySelector('span');
-let imgBox = document.querySelector('.img-box');
-
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext('2d');
-
-function resetValue(){
-  img.style.filter = 'none';
-  saturate.value = '100'
-  contrast.value = '100'
-  brightness.value = '100'
-  sepia.value = '0'
-  grayscale.value = '0'
-  blur.value = '0'
-  hueRotate.value = '0'
-}
-
-
-
-window.onload = function() {
-  download.style.display = 'none';
-  reset.style.display = 'none';
-  imgBox.style.display = 'none';
-}
-upload.onchange = function() {
-  resetValue();
-  download.style.display = 'block';
-  reset.style.display = 'block';
-  imgBox.style.display = 'block';
-  let file = new FileReader();
-  file.readAsDataURL(upload.files[0]);
-  file.onload = function() {
-    img.src = file.result;
-  }
-  img.onload = function() {
-    canvas.width =  img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img,0,0,canvas.width,canvas.height)
-    img.style.display = "none";
-  }
-}
-let filters = document.querySelectorAll("ul li input");
-filters.forEach( filter => {
-  filter.addEventListener('input', function(){
-    ctx.filter = `
+let saturate=document.getElementById("saturate"),contrast=document.getElementById("contrast"),brightness=document.getElementById("brightness"),sepia=document.getElementById("sepia"),grayscale=document.getElementById("grayscale"),blur=document.getElementById("blur"),hueRotate=document.getElementById("hue-rotate"),upload=document.getElementById("upload"),download=document.getElementById("download"),img=document.getElementById("img"),reset=document.querySelector("span"),imgBox=document.querySelector(".img-box"),canvas=document.getElementById("canvas"),ctx=canvas.getContext("2d");function resetValue(){img.style.filter="none",saturate.value="100",contrast.value="100",brightness.value="100",sepia.value="0",grayscale.value="0",blur.value="0",hueRotate.value="0"}window.onload=function(){download.style.display="none",reset.style.display="none",imgBox.style.display="none"},upload.onchange=function(){resetValue(),download.style.display="block",reset.style.display="block",imgBox.style.display="block";let e=new FileReader;e.readAsDataURL(upload.files[0]),e.onload=function(){img.src=e.result},img.onload=function(){canvas.width=img.width,canvas.height=img.height,ctx.drawImage(img,0,0,canvas.width,canvas.height),img.style.display="none"}};let filters=document.querySelectorAll("ul li input");filters.forEach(e=>{e.addEventListener("input",function(){ctx.filter=`
       saturate(${saturate.value}%)
       contrast(${contrast.value}%)
       brightness(${brightness.value}%)
@@ -62,12 +6,4 @@ filters.forEach( filter => {
       grayscale(${grayscale.value})
       blur(${blur.value}px)
       hue-rotate(${hueRotate.value}deg)
-    `
-    ctx.drawImage(img,0,0,canvas.width,canvas.height)
-  })
-} )
-
-
-download.onclick = function(){
-  download.href = canvas.toDataURL()
-}
+    `,ctx.drawImage(img,0,0,canvas.width,canvas.height)})}),download.onclick=function(){download.href=canvas.toDataURL()};
